e68f3209108476f38c940527db5f3a7b53ec4af1
Sylwia <zawada.sylwiaa@gmail.com>
2020-02-24 21:58:05 +0100

    Updates

---
+++ precache-manifest.5794e912f8bc1555d37de83eee18735c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e900ac93ef38e1470068649b73424dc7",
+    "url": "/react-retro-copy/index.html"
+  },
+  {
+    "revision": "84b621ced1354cda4f70",
+    "url": "/react-retro-copy/static/css/main.5a32f71c.chunk.css"
+  },
+  {
+    "revision": "48b1ec6c37a4703a63ed",
+    "url": "/react-retro-copy/static/js/2.b2105ccc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-retro-copy/static/js/2.b2105ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b621ced1354cda4f70",
+    "url": "/react-retro-copy/static/js/main.006d2fe9.chunk.js"
+  },
+  {
+    "revision": "91dc85dc28a854bd86e2",
+    "url": "/react-retro-copy/static/js/runtime-main.38ca7f61.js"
+  }
+]);
