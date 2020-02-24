fdcc840a777977778fd3d3ab155dcdd50e3aed33
Sylwia <zawada.sylwiaa@gmail.com>
2020-02-24 21:54:00 +0100

    Move btn and h4 from Droppable container

--- src/App/components/Board/Column/Column.jsx
+++ src/App/components/Board/Column/Column.jsx
@@ -6,7 +6,7 @@ import { removeColumn } from "../../../store/actions/index";
 import { connect } from "react-redux";
 import { Droppable, Draggable } from "react-beautiful-dnd";
 import "./column.scss";
-import {FaTrashAlt} from 'react-icons/fa'
+import { FaTrashAlt } from "react-icons/fa";
 
 const Column = ({ name, id, notes, dispatch, index }) => {
   const handleRemoveColumn = () => {
@@ -22,20 +22,20 @@ const Column = ({ name, id, notes, dispatch, index }) => {
           ref={provided.innerRef}
           {...provided.dragHandleProps}
         >
+          <h4 className="column-name">{name}</h4>
+          <button className="remove-btn col" onClick={handleRemoveColumn}>
+            <FaTrashAlt className="trash-icon" />
+          </button>
           <Droppable droppableId={id}>
             {provided => (
-              <div {...provided.droppableProps} ref={provided.innerRef}>
-                <h4 className="column-name">{name}</h4>
-                <button className="remove-btn col" onClick={handleRemoveColumn}>
-                  <FaTrashAlt
-                    className="trash-icon"
-                  />
-                </button>
-                <div className="notes-container">
-                  {notes.map((note, index) => (
-                    <Note key={note.id} {...note} index={index} columnId={id} />
-                  ))}
-                </div>
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="notes-container"
+              >
+                {notes.map((note, index) => (
+                  <Note key={note.id} {...note} index={index} columnId={id} />
+                ))}
                 {provided.placeholder}
                 <AddButton columnId={id} btnStyle="create-note" />
               </div>
