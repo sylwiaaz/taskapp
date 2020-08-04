12051a8abe349f0cfaca7d11a8c00e2ccf92d9ee
Sylwia <zawada.sylwiaa@gmail.com>
2020-08-04 15:02:41 +0200

    Updates

---
+++ precache-manifest.cc275e3793fa0e4bb5a4c91b566943cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b98f037b3a36ef792b6ef1f5a1bbb431",
+    "url": "/taskapp/index.html"
+  },
+  {
+    "revision": "6694c390bc69873e04a6",
+    "url": "/taskapp/static/css/main.f3187a2b.chunk.css"
+  },
+  {
+    "revision": "b83510a36f2091691f0c",
+    "url": "/taskapp/static/js/2.5cd1dcdc.chunk.js"
+  },
+  {
+    "revision": "ada05ec494e59967724f97537babe2eb",
+    "url": "/taskapp/static/js/2.5cd1dcdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6694c390bc69873e04a6",
+    "url": "/taskapp/static/js/main.7642aadf.chunk.js"
+  },
+  {
+    "revision": "7daf432bae8be9d8a6a9",
+    "url": "/taskapp/static/js/runtime-main.714a7d54.js"
+  }
+]);
